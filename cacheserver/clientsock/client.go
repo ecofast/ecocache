9dfd2a65579f42dbfd8865e073f206fb00100013
无尽愿 <adelphicoder@gmail.com>
2018-04-27 18:45:22 +0800

    Create client.go

---
+++ cacheserver/clientsock/client.go
@@ -0,0 +1,105 @@
+package clientsock
+
+import (
+	"cacheserver/cfgmgr"
+	"errors"
+	"fmt"
+	"time"
+
+	. "protocols"
+
+	"tcpsock.v2"
+)
+
+type FnWrite = func(b []byte) (n int, err error)
+type FnClose = func() error
+
+type client struct {
+	conn       *tcpsock.TcpConn
+	onWrite    FnWrite
+	onClose    FnClose
+	recvBuf    []byte
+	recvBufLen int
+}
+
+func newClient(conn *tcpsock.TcpConn, fnWrite FnWrite, fnClose FnClose) *client {
+	return &client{
+		conn:    conn,
+		onWrite: fnWrite,
+		onClose: fnClose,
+	}
+}
+
+func (self *client) SockHandle() uint64 {
+	return self.conn.ID()
+}
+
+func (self *client) Read(b []byte) (n int, err error) {
+	count := len(b)
+	if count+self.recvBufLen > tcpsock.RecvBufLenMax {
+		return 0, errors.New("invalid data")
+	}
+
+	self.recvBuf = append(self.recvBuf, b[0:count]...)
+	self.recvBufLen += count
+	offsize := 0
+	offset := 0
+	var head Head
+	for self.recvBufLen-offsize >= SizeOfPacketHead {
+		offset = 0
+		head.Len = uint32(uint32(self.recvBuf[offsize+3])<<24 | uint32(self.recvBuf[offsize+2])<<16 | uint32(self.recvBuf[offsize+1])<<8 | uint32(self.recvBuf[offsize+0]))
+		offset += 4
+		head.Cmd = uint8(self.recvBuf[offsize+offset+0])
+		offset += 1
+		// head.Reserved = uint8(self.recvBuf[offsize+offset+0])
+		offset += 1
+		head.Param = uint16(uint16(self.recvBuf[offsize+offset+1])<<8 | uint16(self.recvBuf[offsize+offset+0]))
+		offset += 2
+		pkglen := int(SizeOfPacketHead + head.Len)
+		if pkglen >= tcpsock.RecvBufLenMax {
+			offsize = self.recvBufLen
+			break
+		}
+		if offsize+pkglen > self.recvBufLen {
+			break
+		}
+		self.process(head.Cmd, head.Param, self.recvBuf[offsize+offset:offsize+offset+int(head.Len)])
+		offsize += SizeOfPacketHead
+	}
+
+	self.recvBufLen -= offsize
+	if self.recvBufLen > 0 {
+		self.recvBuf = self.recvBuf[offsize : offsize+self.recvBufLen]
+	} else {
+		self.recvBuf = nil
+	}
+	return len(b), nil
+}
+
+func (self *client) process(cmd uint8, param uint16, body []byte) {
+	self.conn.RawConn().SetReadDeadline(time.Now().Add(time.Duration(cfgmgr.ClientReadDeadline()) * time.Second))
+	switch cmd {
+	case CM_PING:
+		self.Write(NewPacket(SM_PING, 0, nil).Bytes())
+	case CM_GET:
+		self.processGet(param, body)
+	default:
+		fmt.Println("?????")
+		self.Close()
+	}
+}
+
+// slot name
+// key name
+func (self *client) processGet(param uint16, body []byte) {
+	// param: len(slotname)
+	//
+}
+
+func (self *client) Write(b []byte) (n int, err error) {
+	return self.onWrite(b)
+}
+
+func (self *client) Close() error {
+	return self.onClose()
+}
