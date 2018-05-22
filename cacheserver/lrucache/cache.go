af0858d1897d08af1fb79d5334e1b7159597371c
无尽愿 <adelphicoder@gmail.com>
2018-05-22 17:41:43 +0800

    Update cache.go

--- cacheserver/lrucache/cache.go
+++ cacheserver/lrucache/cache.go
@@ -1,19 +1,22 @@
 package lrucache
 
 import (
+	"cacheserver/errcode"
 	"cacheserver/msgnode"
+	"cacheserver/utils"
 	"lru"
 	. "protocols"
 	"sync"
-	"unsafe"
 )
 
-func newCache(cacheBytes int, msgChan <-chan *msgnode.MsgNode) *cache {
+func newCache(index int, maxEntries, cacheBytes int, msgChan <-chan *msgnode.MsgNode) *cache {
 	c := &cache{
+		index:      index,
 		cacheBytes: cacheBytes,
 		msgChan:    msgChan,
 	}
 	c.lru = &lru.Cache{
+		MaxEntries: maxEntries,
 		OnEvicted: func(key lru.Key, value lru.Value) {
 			c.numBytes -= len(key) + len(value)
 		},
@@ -21,72 +24,64 @@ func newCache(cacheBytes int, msgChan <-chan *msgnode.MsgNode) *cache {
 	return c
 }
 
-// cache is a wrapper around an *lru.Cache that adds synchronization
 type cache struct {
+	index      int
 	lru        *lru.Cache
 	numBytes   int // of all keys and values
 	cacheBytes int
 	msgChan    <-chan *msgnode.MsgNode
 }
 
-func bytes2str(b []byte) string {
-	return *(*string)(unsafe.Pointer(&b))
-}
-
 func (self *cache) run(exitChan chan struct{}, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	self.warmup()
 	for {
 		select {
 		case <-exitChan:
 			self.lru.Clear()
 			return
 		case msg := <-self.msgChan:
-			if msg.Consumer != nil && msg.Cmd == CM_GET {
-				if v, ok := self.get(bytes2str(msg.Body)); ok {
-					msg.Consumer <- msgnode.New(nil, SM_GET, msg.Param, v)
-				} else {
-					msg.Consumer <- msgnode.New(nil, SM_GET, msg.Param, nil)
+			if msg.Consumer != nil {
+				switch msg.Cmd {
+				case CM_GET:
+					sz := len(msg.Body)
+					v, ok := self.get(utils.BytesToStr(msg.Body[uint8(msg.Param>>8) : uint8(msg.Param>>8)+uint8(msg.Param)]))
+					if ok {
+						sz += len(v)
+					}
+					buf := make([]byte, sz)
+					copy(buf, msg.Body)
+					if ok {
+						copy(buf[len(msg.Body):], v)
+						msg.Consumer <- msgnode.New(nil, SM_GET, 0, msg.Param, buf)
+						break
+					}
+					msg.Consumer <- msgnode.New(nil, SM_GET, errcode.ErrKeyDoNotExists, msg.Param, buf)
+				case CM_SET:
+					self.set(utils.BytesToStr(msg.Body[uint8(msg.Param>>8):uint8(msg.Param>>8)+uint8(msg.Param)]), msg.Body[uint8(msg.Param>>8)+uint8(msg.Param):])
+					msg.Consumer <- msgnode.New(nil, SM_SET, 0, msg.Param, msg.Body)
 				}
 			}
 		}
 	}
 }
 
-func (self *cache) warmup() {
-	// to do
-	//
-}
-
 func (self *cache) checkBytes(extra int) {
 	for {
-		if self.numBytes <= self.cacheBytes-extra {
+		if self.numBytes <= self.cacheBytes-extra || self.lru.Len() == 0 {
 			return
 		}
 		self.lru.RemoveOldest()
 	}
 }
 
-func (self *cache) add(key string, value []byte) {
+func (self *cache) set(key string, value []byte) {
+	self.lru.Remove(key)
+	self.checkBytes(len(key) + len(value))
 	self.lru.Add(key, value)
 	self.numBytes += len(key) + len(value)
 }
 
-func (self *cache) load(key string) (value []byte, ok bool) {
-	// to do
-	//
-	return nil, false
-}
-
 func (self *cache) get(key string) (value []byte, ok bool) {
-	if v, ok := self.lru.Get(key); ok {
-		return v, ok
-	}
-	if v, ok := self.load(key); ok {
-		self.checkBytes(len(key) + len(v))
-		self.add(key, v)
-		return v, ok
-	}
-	return nil, false
+	return self.lru.Get(key)
 }
