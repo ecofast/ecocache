333abdfa3816eefc003ed66e6f1928d54bd1f26b
无尽愿 <adelphicoder@gmail.com>
2018-05-22 17:42:51 +0800

    Update msgnode.go

--- cacheserver/msgnode/msgnode.go
+++ cacheserver/msgnode/msgnode.go
@@ -3,17 +3,18 @@ package msgnode
 type MsgNode struct {
 	Consumer chan<- *MsgNode
 	Cmd      uint8
-	_        uint8
+	Ret      uint8
 	Param    uint16
 	_        uint32
 	Body     []byte
 	Next     *MsgNode
 }
 
-func New(consumer chan<- *MsgNode, cmd uint8, param uint16, body []byte) *MsgNode {
+func New(consumer chan<- *MsgNode, cmd, ret uint8, param uint16, body []byte) *MsgNode {
 	return &MsgNode{
 		Consumer: consumer,
 		Cmd:      cmd,
+		Ret:      ret,
 		Param:    param,
 		Body:     body,
 	}
